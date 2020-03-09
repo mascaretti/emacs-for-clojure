29451d69bf46e35e199127d93ae53f54dfe6dd01
Andrea Mascaretti <mascaretti@posteo.net>
2020-03-10 00:37:48 +0100

    removed clojure, should change name

--- init.el
+++ init.el
@@ -16,82 +16,15 @@
 
 
 ;; PACKAGES
-;; Clojure
-(straight-use-package 'cider)
 (straight-use-package 'magit)
 (straight-use-package 'paredit)
-(straight-use-package 'clojure-mode)
-(straight-use-package 'clojure-mode-extra-font-locking)
-(straight-use-package 'ido-completing-read+)
-(straight-use-package 'smex)
 (straight-use-package 'projectile)
 (straight-use-package 'rainbow-delimiters)
-(straight-use-package 'tagedit)
+
 
 ;; Org Journal
 (straight-use-package 'org-journal)
 (customize-set-variable 'org-journal-dir "~/org/journal/")
 (customize-set-variable 'org-journal-date-format "%A, %d %B %Y")
+(customize-set-variable 'org-journal-file-type 'year)
 (require 'org-journal)
-
-;; Place downloaded elisp files in ~/emacs-for-clojure/vendor. You'll then be able
-;; to load them.
-;;
-;; For example, if you download yaml-mode.el to ~/emacs-for-clojure/vendor,
-;; then you can add the following code to this file:
-;;
-;; (require 'yaml-mode)
-;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-;; 
-;; Adding this code will make Emacs enter yaml mode whenever you open
-;; a .yml file
-(add-to-list 'load-path "~/emacs-for-clojure/vendor")
-
-
-;;;;
-;; Customization
-;;;;
-
-;; Add a directory to our load path so that when you `load` things
-;; below, Emacs knows where to look for the corresponding file.
-(add-to-list 'load-path "~/emacs-for-clojure/customizations")
-
-;; Sets up exec-path-from-shell so that Emacs will use the correct
-;; environment variables
-(load "shell-integration.el")
-
-;; These customizations make it easier for you to navigate files,
-;; switch buffers, and choose options from the minibuffer.
-(load "navigation.el")
-
-;; These customizations change the way emacs looks and disable/enable
-;; some user interface elements
-(load "ui.el")
-
-;; These customizations make editing a bit nicer.
-(load "editing.el")
-
-;; Hard-to-categorize customizations
-(load "misc.el")
-
-;; For editing lisps
-(load "elisp-editing.el")
-
-;; Langauage-specific
-(load "setup-clojure.el")
-(load "setup-js.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(package-selected-packages
-   (quote
-    (magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
